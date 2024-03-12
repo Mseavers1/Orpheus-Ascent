08e5e959a844db896cdb75a12a1191ae82fff697
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2024-03-12 03:58:29 -0500

    Implemented basic lava

---
+++ Csc-275-project/scripts/Lava_Logic.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+# Constraints
+var minLavaSpeed = 2
+var maxLavaSpeed = 110
+
+# Vars
+var time = 0
+var lavaSpeed = 0
+
+func _start():
+	lavaSpeed = minLavaSpeed
+
+# Functions that controls how fast the lava goes
+func _speed_function_linear(slope):
+	lavaSpeed = minLavaSpeed + (slope * time)
+	
+func _speed_function_log(slope, inside):
+	lavaSpeed = minLavaSpeed + (slope * log(inside * (time + 1)))
+	
+func _speed_function_pow(slope, exp):
+	lavaSpeed = minLavaSpeed + (slope * (pow(time, exp)))
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y -= delta * lavaSpeed
+	time += delta
+	
+	if lavaSpeed < maxLavaSpeed:
+		_speed_function_linear(0.5)
