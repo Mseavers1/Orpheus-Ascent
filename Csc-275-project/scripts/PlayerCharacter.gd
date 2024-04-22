c4186267cf4f7968de9244a8cafce93f789bf590
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2024-04-22 12:52:00 -0500

    adjustable jumps

--- Csc-275-project/scripts/PlayerCharacter.gd
+++ Csc-275-project/scripts/PlayerCharacter.gd
@@ -2,12 +2,21 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+var max_jump_velocity
+var min_jump_velocity
+
 var jump_count = 0;
+var gravity
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+## Kinematic Jumpings from https://www.youtube.com/watch?v=918wFTru2-c
+@export var max_jump_height = 3.25 * 64
+@export var min_jump_height = 0.5 * 64
+@export var jump_duration = 0.5
 
+func _ready():
+	gravity = 2 * max_jump_height / pow(jump_duration, 2)
+	max_jump_velocity = -sqrt(2 * gravity * max_jump_height)
+	min_jump_velocity = -sqrt(2 * gravity * min_jump_height)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,9 +25,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or jump_count < 2):
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or jump_count < 2):
 		jump_count += 1;
-		velocity.y = JUMP_VELOCITY
+		velocity.y = max_jump_velocity
+	
+	if Input.is_action_just_released("jump") && velocity.y < min_jump_velocity:
+		velocity.y = min_jump_velocity
 		
 	if is_on_floor():
 		$Sprite.play("idle");
@@ -26,7 +38,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move-left", "move-right")
 	
 	if direction < 0:
 		$Sprite.flip_h = true
