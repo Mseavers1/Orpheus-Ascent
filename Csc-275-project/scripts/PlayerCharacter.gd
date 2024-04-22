046b3282bb414998a7708e597c8a06487f66918c
Noah Sternberg <noahcs2002@gmail.com>
2024-04-21 20:51:18 -0500

    Implement basic player

--- Csc-275-project/scripts/PlayerCharacter.gd
+++ Csc-275-project/scripts/PlayerCharacter.gd
@@ -1,32 +1,42 @@
-extends RigidBody2D
-@export var speed = 400;
-var screen_size;
+extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO;
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+var jump_count = 0;
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		$Sprite.play("jump");
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or jump_count < 2):
+		jump_count += 1;
+		velocity.y = JUMP_VELOCITY
+		
+	if is_on_floor():
+		$Sprite.play("idle");
+		jump_count = 0;
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
 	
-	if Input.is_action_pressed("move-left") :
-		velocity.x -= 1;
+	if direction < 0:
+		$Sprite.flip_h = true
+
+	if direction > 0:
+		$Sprite.flip_h = false
 		
-	if Input.is_action_pressed("move-right") :
-		velocity.x += 1;
-		
-	if Input.is_action_pressed("move-down") :
-		velocity.y += 1;
-		
-	if Input.is_action_pressed("jump") :
-		velocity.y -= 1;
-		
-	if velocity.length() > 0 :
-		velocity = velocity.normalized() * speed;
-		
-	position += velocity*delta;
-	# position = position.clamp(Vector2.ZERO, screen_size)
-		
-	#if velocity.y >= 1:
-	#	velocity -= 1;
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
